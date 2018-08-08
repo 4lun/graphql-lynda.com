c967e0dbfe099da1c501250eb0eed6afb53120b4
Alun Davey <hey@4lun.io>
2018-08-08 20:01:22 +0100

    First graphql query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import graphqlHttp from "express-graphql";
+import schema from "./schema";
 
 const PORT = 8080;
 const app = express();
@@ -6,5 +8,13 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("GraphQL is amazing!");
 });
+
+const rootValue = { hello: () => "Hi, I'm Alun" };
+
+app.use("/graphql", graphqlHttp({
+  schema,
+  rootValue,
+  graphiql: true
+}));
 
 app.listen(PORT, () => console.log(`Running server on port http://localhost:${PORT}`));
